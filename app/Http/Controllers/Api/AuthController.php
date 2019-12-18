93acc845c120dfaf2eabc85739e8e5792a56f0b2
zrt <1042252577@qq.com>
2019-12-18 17:36:09 +0800

    f

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+
+class AuthController extends Controller
+{
+    /**
+     * create user
+     * @param [string] name
+     * @param [string] email
+     * @param [string] password
+     * @return [string] message
+     */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string'
+        ]);
+
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $user->save();
+
+        return response()->json([
+            'message' => '用户创建成功'
+        ], 201);
+
+    }
+
+    /**
+     * login user and create token
+     *
+     * @param [string] email
+     * @param [string] password
+     * @param [boolean] remember_me
+     * @return [string] access_token
+     * @return [string] token_type
+     * @return [string] expires_at
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+            'remember_me' => 'boolean'
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => '用户名或密码错误'
+            ], 401);
+        }
+
+        $user = $request->user();
+        // 密码授权访问令牌 ->
+        // 以下使用个人访问令牌 -> https://learnku.com/docs/laravel/6.x/passport/5152#personal-access-tokens
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+        Log::error($tokenResult);
+        Log::error($tokenResult->token);
+        Log::error($tokenResult->accessToken);
+        if ($request->remember_me)
+            $token->expires_at = Carbon::now()->addWeeks(1);
+
+        $token->save();
+
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
+        ]);
+    }
+
+    public function userInfo(Request $request)
+    {
+        return response()->json($request->user());
+    }
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // 'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        // 这个函数会注册发出访问令牌并撤销访问令牌、客户端和个人访问令牌所必需的路由
+        Passport::routes();
+    }
+}
